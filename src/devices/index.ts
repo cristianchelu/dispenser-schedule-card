4faa57811f9c0141d06a5df8cfea4f6a30c9330e
Cristian Chelu <cristianchelu@gmail.com>
2026-04-27 19:58:22 +0300

    feat: add initial PetKit support

--- src/devices/index.ts
+++ src/devices/index.ts
@@ -1,6 +1,7 @@
 import { Device } from "../types/common";
 import { HomeAssistant } from "../types/ha";
 import CustomDevice from "./CustomDevice";
+import PetKitDevice from "./PetKitDevice";
 import PetLibroDevice from "./PetLibroDevice";
 import XiaomiSmartFeeder from "./XiaomiSmartFeeder";
 import XiaomiSmartPetFeeder2 from "./XiaomiSmartPetFeeder2";
@@ -13,16 +14,19 @@ export type {
   PetLibroDeviceConfig,
   PetLibroGlobalToggleConfig,
 } from "./PetLibroDevice";
+export type { PetKitDeviceConfig } from "./PetKitDevice";
 export type { XiaomiSmartFeederDeviceConfig } from "./XiaomiSmartFeeder";
 export type { XiaomiSmartPetFeeder2DeviceConfig } from "./XiaomiSmartPetFeeder2";
 
 import type { CustomDeviceConfig } from "./CustomDevice";
+import type { PetKitDeviceConfig } from "./PetKitDevice";
 import type { PetLibroDeviceConfig } from "./PetLibroDevice";
 import type { XiaomiSmartFeederDeviceConfig } from "./XiaomiSmartFeeder";
 import type { XiaomiSmartPetFeeder2DeviceConfig } from "./XiaomiSmartPetFeeder2";
 
 export type DeviceConfig =
   | CustomDeviceConfig
+  | PetKitDeviceConfig
   | PetLibroDeviceConfig
   | XiaomiSmartFeederDeviceConfig
   | XiaomiSmartPetFeeder2DeviceConfig;
@@ -38,6 +42,8 @@ export function createDevice(
       return new XiaomiSmartFeeder(deviceConfig, hass);
     case "xiaomi-smart-feeder-2":
       return new XiaomiSmartPetFeeder2(deviceConfig, hass);
+    case "petkit":
+      return new PetKitDevice(deviceConfig, hass);
     case "petlibro":
       return new PetLibroDevice(deviceConfig, hass);
     case "custom":
